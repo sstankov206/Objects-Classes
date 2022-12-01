3786a203d796708bf1cfb749b90ac065138c4705
Stan S <stankovst@gmail.com>
2022-12-01 14:14:41 -0800

    adding OrderByAge

---
+++ OrderByAge/OrderByAge/OrderByAge/Models/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace OrderByAge.Models
+{
+	public class Person
+	{
+		public string Name { get; set; }
+		public string ID { get; set; }
+		public int Age { get; set; }
+		
+
+		public Person(string name, string id, int age)
+		{
+			this.Name = name;
+			this.ID = id;
+			this.Age = age;
+
+		}
+
+		public Person()
+		{ 
+        }
+
+
+        //override ToString() :
+
+        public override string ToString()
+		{
+
+			return $"{Name} with ID: {ID} is {Age} years old.";
+        }
+    }
+}
+
---
+++ OrderByAge/OrderByAge/OrderByAge/Program.cs
@@ -0,0 +1,39 @@
+﻿//solution by me, sstankov
+using OrderByAge.Models;
+using System.Text;
+
+//create a list of the 'Person' class to store new 'people' :
+List<Person> people = new List<Person>();
+
+//reading input:
+string command = Console.ReadLine();
+
+//while input ISN'T = "End" :
+while (!command.Equals("End"))
+{
+    //create array from input, split by space:
+    string[] input = command.Split();
+
+    //create new instance of 'person':
+    Person person = new Person();
+
+    person.Name = input[0]; //get person's name
+    person.ID = input[1]; //get person's ID
+    person.Age = int.Parse(input[2]); //get person's age
+
+    //add new person to the list 'people' :
+    people.Add(person);
+
+    command = Console.ReadLine();
+}
+
+//output:
+people
+    .OrderBy(person=>person.Age)
+    .ToList()
+
+    .ForEach(person=>
+Console.WriteLine(person.ToString()));
+
+var result = String.Join(Environment.NewLine, people);
+Console.WriteLine(result);
