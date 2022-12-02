7bf7bfd4c77f70ec64624dd94dbc1511b2af0284
Stan S <stankovst@gmail.com>
2022-12-01 22:16:41 -0800

    adding CompanyRoster

---
+++ CompanyRoster/CompanyRoster/CompanyRoster/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace CompanyRoster.Models
+{
+	public class Employee
+	{
+		public string  Name { get; set; }
+		public decimal Salary { get; set; }
+		public string Department { get; set; }
+
+		public Employee(string name, string salary, string department)
+		{
+			this.Name = name;
+			this.Salary = decimal.Parse(salary);
+			this.Department = department;
+		}
+
+		public Employee()
+		{
+		}
+
+        public override string ToString()
+        {
+            return $"{Name} {Salary}";
+        }
+    }
+}
+
---
+++ CompanyRoster/CompanyRoster/CompanyRoster/Program.cs
@@ -0,0 +1,35 @@
+﻿//solution by me, sstankov.
+using System.Linq;
+using CompanyRoster.Models;
+
+//determine 'N' number of lines of employees:
+int n = int.Parse(Console.ReadLine());
+
+//create List to store the employees in:
+List<Employee> employees = new List<Employee>();
+
+
+//iterate through the array:
+for (int i = 0; i < n; i++)
+{
+    //create an array 'roster' w/ size of 'N' and split by space:
+    string[] roster = new string[n];
+    roster = Console.ReadLine().Split();
+
+    //create new employee each iteration:
+    Employee employee = new Employee(roster[0], roster[1], roster[2]); 
+    
+    employees.Add(employee); //add each newly create employee to the list 'employees'
+
+}
+
+IGrouping<string, Employee> grouping = employees
+    .GroupBy(employee => employee.Department)
+    .OrderByDescending(x => x.Sum(y => y.Salary))
+    .First();
+   
+Console.WriteLine($"Highest Average Salary: {grouping.Key}\n{String.Join("\n", grouping.OrderByDescending(e=>e.Salary))}");
+
+
+
+
